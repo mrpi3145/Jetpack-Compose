568c602e5ea82b519c9da55d7e92ba551b126371
mrpi <mrpi3145@gmail.com>
2022-04-07 17:07:23 +0530

    Styling Text

--- app/src/main/java/com/windapp/composecourse/MainActivity.kt
+++ app/src/main/java/com/windapp/composecourse/MainActivity.kt
@@ -20,7 +20,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,14 +38,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val painter= painterResource(id = R.drawable.kermit)
-            val description="Kemit in love"
-            val title=description
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF1E1F1D))
 
-            Box(modifier = Modifier.fillMaxSize(0.5f).padding(12.dp)){
-                ImageCard(painter = painter,
-                    contentDescription = description,
-                    title =title)
+            ){
+                Text(text= buildAnnotatedString {
+                                                withStyle(
+                                                    style = SpanStyle(
+                                                        color=Color.Green,
+                                                        fontSize = 50.sp
+                                                    )
+                                                ){
+                                                    append("J")
+                                                }
+                    append("etpack")
+
+                    withStyle(
+                        style = SpanStyle(
+                            color=Color.Green,
+                            fontSize = 50.sp
+                        )
+                    ){
+                        append("C")
+                    }
+                    append("ompose")
+                },
+                color=Color.White,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+
+                    )
             }
 
 
@@ -50,60 +83,4 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun ImageCard(
-    painter:Painter,
-    contentDescription:String,
-    title:String,
-    modifier: Modifier=Modifier
 
-){
-
-    Card(
-        modifier = modifier.fillMaxSize(),
-        shape= RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ){
-        Box(modifier = Modifier.height(200.dp)){
-            Image(
-                painter=painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Brush.verticalGradient(colors = listOf(Color.Transparent, Color.Yellow),
-                startY = 300f
-                    ))
-
-            )
-
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-            contentAlignment = Alignment.BottomStart) {
-
-                Text(title,style = TextStyle(color= Color.Green,fontSize = 12.sp))
-
-            }
-        }
-
-    }
-
-
-}
-
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCourseTheme {
-        Greeting("Mahadeva")
-    }
-}

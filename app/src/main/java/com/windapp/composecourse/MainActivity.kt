86f04d50d9442fd3d102417a9bae1e6c9d58ef24
mrpi <mrpi3145@gmail.com>
2022-11-05 11:15:21 +0530

    Textfields, Buttons & Showing Snackbars - Android Jetpack Compose

--- app/src/main/java/com/windapp/composecourse/MainActivity.kt
+++ app/src/main/java/com/windapp/composecourse/MainActivity.kt
@@ -3,14 +3,14 @@ package com.windapp.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,40 +32,61 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.windapp.composecourse.ui.theme.ComposeCourseTheme
 import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
+            
+            val scaffoldState= rememberScaffoldState()
 
-            LazyColumn{
-                itemsIndexed(
-                    listOf("this","is","jetpack","compose")
-                ){
-                    index,string ->
-
-                    Text(
-                        text=string,
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
-
-                }
+            var textFieldState by remember {
+                mutableStateOf("")
             }
+            val scope= rememberCoroutineScope()
+
+ Scaffold(
+     modifier = Modifier.fillMaxSize(),
+     scaffoldState =scaffoldState
+ ) {
+     Column(
+         horizontalAlignment = Alignment.CenterHorizontally,
+         verticalArrangement = Arrangement.Center,
+         modifier = Modifier
+             .fillMaxSize()
+             .padding(horizontal = 30.dp)
+                 
+     ) {
+       TextField(value = textFieldState,
+           label={
+
+                 Text("Enter your name")
+           },
+           modifier=Modifier.fillMaxWidth(),
+
+           onValueChange ={
+               textFieldState=it
+           } )
+         Spacer(modifier = Modifier.height(16.dp))
+
+         Button(onClick = {
+             scope.launch {
+                 scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+
+             }
+
+         }) {
+             Text("Please greet us")
+         }
+     }
+
+ }
+
 
 
 
         }
     }
-
-
-
 
 }
 
